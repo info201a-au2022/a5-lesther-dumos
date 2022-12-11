57bcbe0e2370e361df9c574e7aba4ac04b0b22fb
Lesther <lestherdumosmog@gmail.com>
2022-12-11 09:36:49 -0800

    process making graph 1

--- a5/server.R
+++ a5/server.R
@@ -17,7 +17,40 @@ library(shinythemes)
 
 co2_df <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv", stringsAsFactors =  FALSE)
 
-# Define server logic required to draw a histogram
+#Graph 2
+per_cap_df <- co2_df %>% 
+  filter(year == "2020") %>% 
+  select(country, cement_co2_per_capita, co2_including_luc_per_capita, co2_per_capita,
+         coal_co2_per_capita, consumption_co2_per_capita, energy_per_capita, flaring_co2_per_capita,
+         gas_co2_per_capita, ghg_per_capita, land_use_change_co2_per_capita, methane_per_capita,
+         nitrous_oxide_per_capita, oil_co2_per_capita, other_co2_per_capita) %>% 
+  rename("region" = "country") %>% 
+  group_by(region)
+
+world_cords <- map_data("world")
+world_cap <- left_join(per_cap_df, world_cords)
+
+#Server
 server <- function (input, output){
- 
+  
+ output$graph1 <- renderPlot({
+   map1 <- ggplot() +
+     geom_polygon(data = world_cap, aes(x = long, y = lat, group = group, fill = fill_flg)) +
+     geom_point(data = world_cap, aes(x = lon, y = lat), color = "#e60000") +
+     scale_fill_manual(values = c("#CCCCCC","#e60000")) +
+     labs(title = 'Countries with highest "talent competitiveness"'
+          ,subtitle = "source: INSEAD, https://www.insead.edu/news/2017-global-talent-competitiveness-index-davos") +
+     theme(text = element_text(family = "Gill Sans", color = "#FFFFFF")
+           ,panel.background = element_rect(fill = "#444444")
+           ,plot.background = element_rect(fill = "#444444")
+           ,panel.grid = element_blank()
+           ,plot.title = element_text(size = 30)
+           ,plot.subtitle = element_text(size = 10)
+           ,axis.text = element_blank()
+           ,axis.title = element_blank()
+           ,axis.ticks = element_blank()
+           ,legend.position = "none"
+     )
+   map1
+ })
 }
--- a5/ui.R
+++ a5/ui.R
@@ -8,6 +8,8 @@
 #
 
 library(shiny)
+library(tidyverse)
+
 
 intro_panel <- tabPanel(
   "Introduction",
@@ -17,20 +19,41 @@ intro_panel <- tabPanel(
   sidebarLayout(
     mainPanel(
     ),
-    sidebarPanel (
-    ) 
+
   )
 )
 
 interactive_1 <- tabPanel(
-  "Interactive 1",
+  "Interactive Page",
   titlePanel("Graph"),
   h3(strong("Introduction")),
   sidebarLayout(
-    mainPanel(
+    sidebarPanel(
+      selectInput(
+        inputId = "graph1_input",
+        label = "Choose varaible",
+        choices = list(
+          "Annual production-based emissions of carbon dioxide" = "cement_co2_per_capita",
+          "Annual production-based emissions of carbon dioxide (CO₂), including land-use change" = "co2_including_luc_per_capita",
+          "Annual total production-based emissions of carbon dioxide (CO₂), excluding land-use change" = "co2_per_capita",
+          "Annual production-based emissions of carbon dioxide (CO₂) from coal" = "coal_co2_per_capita",
+          "Annual consumption-based emissions of carbon dioxide (CO₂)" = "consumption_co2_per_capita",
+          "Primary energy consumption" = "energy_per_capita",
+          "Annual production-based emissions of carbon dioxide (CO₂) from flaring" = "flaring_co2_per_capita",
+          "Annual production-based emissions of carbon dioxide (CO₂) from gas" = "gas_co2_per_capita",
+          "Total greenhouse gas emissions including land-use change and forestry" = "ghg_per_capita",
+          "Annual production-based emissions of carbon dioxide (CO₂) from land-use change" = "land_use_change_co2_per_capita",
+          "Total methane emissions including land-use change and forestry" = "methane_per_capita",
+          "Total nitrous oxide emissions including land-use change and forestry" = "nitrous_oxide_per_capita",
+          "Annual production-based emissions of carbon dioxide (CO₂) from oil" = "oil_co2_per_capita",
+          "Annual production-based emissions of carbon dioxide (CO₂) from other industry sources" = "other_co2_per_capita"
+        ),
+        selected = "cement_co2_per_capita"
+      ),
     ),
-    sidebarPanel (
-    ) 
+    mainPanel(
+      plotOutput("graph1")
+    )
   )
 )
 
